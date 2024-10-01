41f9c9d0c604cc13d852990f1686461c3cc6a046
Giovanna Adorno <69016130+giovanna-adorno@users.noreply.github.com>
2024-09-30 21:26:50 -0300

    Ajustando a lambda para retornar corretamente para o API Gateway

--- modules/lambda/get_function/admin.js
+++ modules/lambda/get_function/admin.js
@@ -2,16 +2,29 @@ const AWS = require('aws-sdk');
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-  const cpf = event.queryStringParameters.cpf;
+  const cpf = event.queryStringParameters?.cpf;
+
+  const response = function (principalId, effect, resource) {
+    let authResponse = {};
+    authResponse.principalId = principalId;
+
+    if (effect && resource) {
+
+      let policyDocument = {};
+      policyDocument.Version = '2012-10-17';
+      policyDocument.Statement = [];
+      let statementOne = {};
+      statementOne.Action = 'execute-api:Invoke';
+      statementOne.Effect = effect;
+      statementOne.Resource = resource;
+      policyDocument.Statement[0] = statementOne;
+      authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+  }
 
   if (!cpf) {
-    return {
-      statusCode: 400,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ message: 'CPF is missing' })
-    };
+    return response('user', 'Deny', event.methodArn);
   }
 
   const userPoolId = process.env.USER_POOL_ID;
@@ -23,25 +36,6 @@ exports.handler = async (event) => {
     const data = await cognito.adminGetUser(params).promise();
     const isAdmin = data.UserAttributes.find(attr => attr.Name === 'custom:isadmin')['Value'];
 
-    const response = function (principalId, effect, resource) {
-      let authResponse = {};
-      authResponse.principalId = principalId;
-
-      if (effect && resource) {
-
-        let policyDocument = {};
-        policyDocument.Version = '2012-10-17';
-        policyDocument.Statement = [];
-        let statementOne = {};
-        statementOne.Action = 'execute-api:Invoke';
-        statementOne.Effect = effect;
-        statementOne.Resource = resource;
-        policyDocument.Statement[0] = statementOne;
-        authResponse.policyDocument = policyDocument;
-      }
-      return authResponse;
-    }
-
     if (isAdmin === 'false') {
       return response('user', 'Deny', event.methodArn);
     }
@@ -49,22 +43,6 @@ exports.handler = async (event) => {
     return response('user', 'Allow', event.methodArn);
 
   } catch (error) {
-    if (error.code === 'UserNotFoundException') {
-      return {
-        statusCode: 404,
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message: 'Usuário não encontrado. Deseja cadastrar e aproveitar nossas promoções?' })
-      };
-    } else {
-      return {
-        statusCode: 500,
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message: 'Error retrieving user', error: error.message })
-      };
-    }
+    return response('user', 'Deny', event.methodArn);
   }
 };
